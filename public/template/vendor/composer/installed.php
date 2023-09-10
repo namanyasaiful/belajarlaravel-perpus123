0b41a8dec6ad2101b391c406b804ca11d19692ad
namanyasaiful <saifullohfattah023@gmail.com>
2023-09-10 21:00:07 +0700

    tambahan

---
+++ public/template/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'almasaeed2010/adminlte',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b42c885edfeb79d4da2a1609840fe64b44480260',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'almasaeed2010/adminlte' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b42c885edfeb79d4da2a1609840fe64b44480260',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('dashboard');
+
+Route::get('/anggota', [PerpustakaanController::class, 'anggota'])->name('get_anggota');
+
+Route::get('/buku', [PerpustakaanController::class, 'buku'])->name('get_buku');
+
+Route::get('/petugas', [PerpustakaanController::class, 'petugas'])->name('get_petugas');
